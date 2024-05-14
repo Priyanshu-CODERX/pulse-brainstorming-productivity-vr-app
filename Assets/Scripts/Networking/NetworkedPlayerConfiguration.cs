9566c7cae6e57632b893876b2ad6ad2afc9a5ae6
Priyanshu Bhattacharjee <59302986+Priyanshu-CODERX@users.noreply.github.com>
2024-05-14 12:00:29 +0530

    fix: hand movement synchronization issue

--- Assets/Scripts/Networking/NetworkedPlayerConfiguration.cs
+++ Assets/Scripts/Networking/NetworkedPlayerConfiguration.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class NetworkedPlayerConfiguration : MonoBehaviour
 {
     public CharacterController XRRigCharacterController;
     public CharacterControllerDriver XRRigCharacterControllerDriver;
+    public XRBaseController XRLeftController;
+    public XRBaseController XRRightController;
     public InputData XRRigInputData;
     public GameObject XRLocomotion;
     public GameObject XRRigCamera;
@@ -15,6 +18,8 @@ public class NetworkedPlayerConfiguration : MonoBehaviour
     {
         XRRigCharacterController.enabled = true;
         XRRigCharacterControllerDriver.enabled = true;
+        XRLeftController.enabled = true;
+        XRRightController.enabled = true;
         XRLocomotion.SetActive(true);
         XRRigInputData.enabled = true;
         XRRigCamera.SetActive(true);
