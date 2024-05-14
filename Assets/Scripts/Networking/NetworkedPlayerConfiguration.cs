d2970476e8523a1e00eb7556af743c8c0f755bb2
Priyanshu Bhattacharjee <59302986+Priyanshu-CODERX@users.noreply.github.com>
2024-05-14 11:39:53 +0530

    fix: fixed locomotion mirroring issue

--- Assets/Scripts/Networking/NetworkedPlayerConfiguration.cs
+++ Assets/Scripts/Networking/NetworkedPlayerConfiguration.cs
@@ -7,12 +7,16 @@ public class NetworkedPlayerConfiguration : MonoBehaviour
 {
     public CharacterController XRRigCharacterController;
     public CharacterControllerDriver XRRigCharacterControllerDriver;
+    public InputData XRRigInputData;
+    public GameObject XRLocomotion;
     public GameObject XRRigCamera;
 
     public void IsLocalUser()
     {
         XRRigCharacterController.enabled = true;
         XRRigCharacterControllerDriver.enabled = true;
+        XRLocomotion.SetActive(true);
+        XRRigInputData.enabled = true;
         XRRigCamera.SetActive(true);
     }
 }
